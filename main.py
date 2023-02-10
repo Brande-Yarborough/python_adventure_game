cf4beb3ebeca29d823a4a6ef23979fdc74b64406
BrandeYarborough <brandeyarborough@gmail.com>
2023-02-10 14:05:28 -0500

    adds start story of game

---
+++ main.py
@@ -0,0 +1,10 @@
+name = input('Enter your name: ')
+print(f'Hello, {name}! Welcome to your adventure!')
+
+# put story about following groundhog here?
+start = input(
+    'Would you like to follow the witty albino groundhog with an Apple watch down the rabbit hole into Wonderland? Yes or No: ')
+if start == 'Yes'.lower:
+    print("Here we go! Get ready to encounter many mythical creatures on your adventure. May the odds be ever in your favor!")
+else:
+    print('The groundhog and Mad Hatter are sad you did not join them :( ')
