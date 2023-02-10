7bae193329f8a2d68ca090836e8f140e55b67fc2
Conor McManamon <c.s.mcmanamon@gmail.com>
2023-02-10 15:53:35 -0500

    Main class created. Not currently being called.

--- main.py
+++ main.py
@@ -16,7 +16,30 @@ class Main:
     def main_play(self):
         conors_room.ConorRoom(self.key).play()
 
+    def start_adventure(self):
+        start = input(
+            'Would you like to follow the witty albino groundhog with an Apple watch down the rabbit hole into Wonderland? Yes or No: ')
+        if start == 'Yes'.lower:
+            print("Here we go! Get ready to encounter many mythical creatures on your adventure. May the odds be ever in your favor!")
+            players_choice = input(
+                'which adventure would you like to start with?')
 
+            if players_choice == 1:
+                print("you have chose to the journey to the kingdom of hearts")
+            elif players_choice == 2:
+                pass
+            else:
+                print("I get that you think you're a special snowflake, but imma really need for you to stick with that status quo here.")
+        else:
+            print('The groundhog and Mad Hatter are sad you did not join them :')
+
+
+#
+# else:
+# print('The groundhog and Mad Hatter are sad you did not join them :( ')
+
+#
+#
 # items = 0
 # class Room():
 #     def __init__(self):
@@ -42,24 +65,6 @@ class Main:
 # print(f'Hello, {name}! Welcome to your adventure!')
 
 # put story about following groundhog here?
-start = input(
-    'Would you like to follow the witty albino groundhog with an Apple watch down the rabbit hole into Wonderland? Yes or No: ')
-if start == 'Yes'.lower:
-    print("Here we go! Get ready to encounter many mythical creatures on your adventure. May the odds be ever in your favor!")
-    players_choice = input(
-        'which adventure would you like to start with?')
-    if players_choice == 1:
-        print("you have chose to the journey to the kingdom of hearts")
-        amber.check_out()
-    elif players_choice == 2:
-        pass
-    else:
-        print("I get that you think you're a special snowflake, but imma really need for you to stick with that status quo here.")
-
-
-else:
-    print('The groundhog and Mad Hatter are sad you did not join them :( ')
-
 
 # items = 0
 # class Room():
