f302710083a93b9fbfb5b78423e87a6c9b6243b6
Conor McManamon <c.s.mcmanamon@gmail.com>
2023-02-10 14:59:31 -0500

    Test functions for Conor

--- conors_room.py
+++ conors_room.py
@@ -1,1 +1,10 @@
-print('conor')
+class ConorRoom(Object):
+    def __init__(self):
+        self.health = 100
+        self.riddle_correct = False
+
+    def check_out(self):
+        print("Hello traveler!")
+
+    def entrance(self):
+        print("welcome to the beach \n the ocean is a powerful and mysterious creature")
