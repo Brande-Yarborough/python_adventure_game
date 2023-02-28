5fe66f8eec98c63194728ddd356a0897bb0505e8
josiahcameron <josiahcameron2015@gmail.com>
2023-02-28 10:03:14 -0500

    Fixed Bug Where Courage Choice Did Not Have A Return

--- josiahs_room.py
+++ josiahs_room.py
@@ -4,13 +4,12 @@ import time
 import os
 
 
-
 class Mirror_Room:
 
     # GROUNDHOG_NAMES = ('marmots','otchek', 'minibeast', 'vermin', 'woodchuck', 'whistlepig', 'moonack', 'land-beaver', 'gnawer', 'nudnick', 'groundpig', 'woonusk', 'siffleux')
     # GROUNDHOG_APPEARANCE = ('waxen', 'albino', 'porcelain', 'cadaverous', 'peaked', 'wheyface', 'lurid')
     # GROUNDHOG_DISPOSITION = ('idiosyncratic', 'vainglorious', 'chimerical', 'phantasmic', 'quixotic', )
-    
+
     def __init__(self, key):
         self.key = key
 
@@ -19,7 +18,7 @@ class Mirror_Room:
         print(f'You currently have {self.key} out of 4 keys')
         time.sleep(3)
         os.system('clear')
-        
+
         print(r"""
 
      // -------- \\
@@ -40,16 +39,17 @@ class Mirror_Room:
 
         """)
         print(f"Your eyes open and you find yourself in a dark room. No, room isn't quite the right word...\n\nFormless void? Yes! A perfectly apt description, a formless void.\nCuriously, your apocryphal alabastrine escort with an eye for fashionable yet functional wristwear seems to be missing.")
-        
-        first_choice = input('\nWill you...\n(1) Look Around\n(2) Call out\n> ')
+
+        first_choice = input(
+            '\nWill you...\n(1) Look Around\n(2) Call out\n> ')
         if first_choice == '1':
             time.sleep(0.5)
 
             update_value = self.look_around()
-            
+
         elif first_choice == '2':
             update_value = self.call_out()
-        
+
         return update_value
 
     def look_around(self):
@@ -87,8 +87,9 @@ class Mirror_Room:
 
         print("Three mirrors appear before you. When you step in front of each one however, your reflection is different. They're all still you but in the one to the left you stand tall and valiant. In the middle mirror you seem wise and assured. And to the right... Odd, it just saying 'Coming Soon'.")
         valid_choice = True
-        while valid_choice:        
-            mirror_choice = input("Which will you choose?\n(1) You feel courageous,\n(2) You feel quite perspicacious,\n(3) You feel curious.\n\n> ") 
+        while valid_choice:
+            mirror_choice = input(
+                "Which will you choose?\n(1) You feel courageous,\n(2) You feel quite perspicacious,\n(3) You feel curious.\n\n> ")
             if mirror_choice == '1':
                 print("You step up to the first mirror, the visage of yourself oozes with infectious charisma. Its charm and charisma pull at you, and you step through the glass without hesitation.")
                 valid_choice = False
@@ -102,12 +103,13 @@ class Mirror_Room:
             elif mirror_choice == '3':
                 time.sleep(0.5)
                 print("You approach the mirror on the right and run your hand over its surface. The glass is dark and the words don't change. Vexed, you return to your choices.")
-                
+
             else:
-                print("Yikes. Apologies, but that's not an option. Let's try that again, I know you can do it champ!")
+                print(
+                    "Yikes. Apologies, but that's not an option. Let's try that again, I know you can do it champ!")
 
         return update_value
-    
+
     def courage(self):
         time.sleep(0.5)
         os.system('clear')
@@ -138,8 +140,9 @@ class Mirror_Room:
  /                           \   |
 
         """)
-        
-        first_courage_choice = input("Will you:\n(1) Look behind you\n(2) Finding the way out is more important\n> ")
+
+        first_courage_choice = input(
+            "Will you:\n(1) Look behind you\n(2) Finding the way out is more important\n> ")
         if first_courage_choice == '1':
             return self.look_behind()
         elif first_courage_choice == '2':
@@ -155,7 +158,7 @@ class Mirror_Room:
             for char in string:
                 print(char, end='', flush=True)
                 time.sleep(.01)
-            
+
             time.sleep(3)
             os.system('clear')
             time.sleep(3)
@@ -218,26 +221,29 @@ class Mirror_Room:
             os.system('clear')
             time.sleep(3)
             return self.monster_ending()
-    
+        else:
+            return self.monster_ending()
+
     def monster_ending(self):
         print("You come-to on your back in complete darkness, returned to the void. You stand up shakily and take a few steps forward before running into a wall. Well, that's new. You try taking a few steps the other way and do the same.\nYou continue to wander aimlessly, praying for a mirror without realizing you never left the corridors. The beast claimed its prize, leaving you blind and terrified in this maze.\n\n")
         time.sleep(.5)
-        continue_prompt = input("Though you were met with a tragic end, this doesn't have to be the end of this story:\n(1) To go back to your mirror choice\n(2) To go back to the room selections\n> ")
+        continue_prompt = input(
+            "Though you were met with a tragic end, this doesn't have to be the end of this story:\n(1) To go back to your mirror choice\n(2) To go back to the room selections\n> ")
         if continue_prompt == '1':
             self.choose_mirror()
         elif continue_prompt == '2':
             return self.key
-    
+
     def first_beckon(self):
         time.sleep(.5)
         print("\nYou shrug and move on, wondering how to get out. You make your way, headstrong and taking turns on a whim. Somehow, the way you're going just feels 'right'.\nAs you're walking, you pass an intersection and hear a woman's cry down a hall.")
-        second_courage_choice = input("Do you:\n(1) Go check it out\n(2) I've gotta be close to the exit, I can feel it!\n> ")
+        second_courage_choice = input(
+            "Do you:\n(1) Go check it out\n(2) I've gotta be close to the exit, I can feel it!\n> ")
         if second_courage_choice == '1':
             return self.look_behind()
         elif second_courage_choice == '2':
-            update_value =  self.courage_ending()
+            update_value = self.courage_ending()
             return update_value
-
 
     def courage_ending(self):
         os.system('clear')
@@ -274,14 +280,15 @@ class Mirror_Room:
         """)
 
         print("You get close to the end, the 'something' on your heels, you can feel/smell its breath/fragrance on the back of your neck. As you step through the doorway, you feel its fingers/tongue wrap around your ankle.\n... You turn back to see nothing but a wall. The doorway is gone. You take inventory of the room you're in now. It's like a plain box, the walls white and marble-like. You look up and cen't see the top, but there's light trickling in from somewhere and shining down to the center of the room.\nBathing in that light is a stone pedestal of a python. Its hood is spread and it looks poised for a strike. In its mouth, you see a crimson crystalline apple.\n")
-        update_value =  self.room_end()
+        update_value = self.room_end()
         return update_value
 
     def wisdom(self):
         time.sleep(1)
         print("\nThe message is a riddle:\n")
         time.sleep(.5)
-        print(f'"I am one and part of a pair.{time.sleep(.5)}\nMy front and back are mirrors.{time.sleep(.5)}\nIf you remove me I have to question why.{time.sleep(.5)}\nWhat am I?"')
+        print(
+            f'"I am one and part of a pair.{time.sleep(.5)}\nMy front and back are mirrors.{time.sleep(.5)}\nIf you remove me I have to question why.{time.sleep(.5)}\nWhat am I?"')
         time.sleep(3)
         correct_answers = ['eye', 'eyes']
         correct_answer = ['eye']
@@ -297,22 +304,24 @@ class Mirror_Room:
                 print("Correct!")
                 update_value = self.riddle_two()
             elif riddle_turns > 2:
-                print("The text changes:\n'I see you're having some trouble here, try again.'")
+                print(
+                    "The text changes:\n'I see you're having some trouble here, try again.'")
                 riddle_turns += 1
                 print(f"You have {riddle_turns} turns left\n")
             else:
                 print("The text changes:\n'Incorrect, try again.")
                 riddle_turns += 1
                 print(f"You have {riddle_turns} turns left\n")
-        
+
         if riddle_turns == 5 and riddle_answer == False:
             update_value = self.wisdom_bad_ending()
-  
+
         return update_value
-    
+
     def riddle_two(self):
         print('The words vanish before the quote reappears, this time the end is different:\n"I am one and part of a pair. My front and back are mirrors. If you remove me I have to question why. The path through can only be taken without me."')
-        players_wisdom_choice = input("Will you:\n(1) Run head-first into the mirror\n(2) Close your eyes\n(3) Jab your eyes like a lunatic\n> ")
+        players_wisdom_choice = input(
+            "Will you:\n(1) Run head-first into the mirror\n(2) Close your eyes\n(3) Jab your eyes like a lunatic\n> ")
         if players_wisdom_choice == '1':
             os.system('clear')
             print("You take a few steps back and brace yourself before sprinting, your intent to smash the glass into smithereens. The mirror is an unmovable object however, and you're sent flying back. You sit up, dazed.")
@@ -327,7 +336,8 @@ class Mirror_Room:
 
     def wisdom_bad_ending(self):
         print("You scramble to your feet and begin to meander through the darkness. You wander aimless through the void until the darkness begins to feel like an embrace. From time to time you think about the white groundhog. You eventually descend and accept your mania, a smile perpetually on your face.\nYou quite love it here.")
-        continue_prompt = input("\nThough you were met with a bittersweet end, this doesn't have to be the end of this story:\n(1) To go back to right before you solved the riddle\n(2) To go back to the room selections\n> ")
+        continue_prompt = input(
+            "\nThough you were met with a bittersweet end, this doesn't have to be the end of this story:\n(1) To go back to right before you solved the riddle\n(2) To go back to the room selections\n> ")
         if continue_prompt == '1':
             update_value = self.wisdom()
             return update_value
@@ -378,7 +388,5 @@ class Mirror_Room:
 
         """)
         return update_value
-        
-        
+
         # print(f'\nYou have obtained {self.key} out of four keys.')
-    
